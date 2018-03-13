3f43d0954bdd719933ffca77158b86e9c8b772d8
DMagic <david.grandy@gmail.com>
2018-03-13 15:03:38 -0400

    Unity project

---
+++ Unity/Better Tracking/Assets/Editor/Bundler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+public class Bundler
+{
+	const string dir = "AssetBundles";
+	const string extension = ".btk";
+
+    [MenuItem("BetterTracking/Build Bundles")]
+    static void BuildAllAssetBundles()
+    {
+		BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.StandaloneWindows);
+
+		FileUtil.ReplaceFile(dir + "/better_tracking_prefabs", dir + "/better_tracking_prefabs" + extension);
+
+		FileUtil.DeleteFileOrDirectory(dir + "/better_tracking_prefabs");
+	}
+
+
+}
