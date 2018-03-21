07d092cc03dc78e3eaf1dc02935fb6ad4b1e7abc
DMagic <david.grandy@gmail.com>
2018-03-21 12:19:54 -0400

    Drag and drop list - from Unity UI Extensions

---
+++ Source/BetterTracking.Unity/Extensions/ReorderableList.cs
@@ -0,0 +1,112 @@
+﻿/// Credit Ziboo
+/// Sourced from - http://forum.unity3d.com/threads/free-reorderable-list.364600/
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace BetterTracking.Unity
+{
+    [RequireComponent(typeof(RectTransform)), DisallowMultipleComponent]
+    [AddComponentMenu("UI/Extensions/Re-orderable list")]
+    public class ReorderableList : MonoBehaviour
+    {
+        [Tooltip("Child container with re-orderable items in a layout group")]
+        public LayoutGroup ContentLayout;
+        [Tooltip("Parent area to draw the dragged element on top of containers. Defaults to the root Canvas")]
+        public RectTransform DraggableArea;
+
+        [Tooltip("Can items be dragged from the container?")]
+        public bool IsDraggable = true;
+        [Tooltip("Should the draggable components be removed or cloned?")]
+        public bool CloneDraggedObject = false;
+
+        [Tooltip("Can new draggable items be dropped in to the container?")]
+        public bool IsDropable = true;
+        
+
+        [Header("UI Re-orderable Events")]
+        public ReorderableListHandler OnElementDropped = new ReorderableListHandler();
+        public ReorderableListHandler OnElementGrabbed = new ReorderableListHandler();
+        public ReorderableListHandler OnElementRemoved = new ReorderableListHandler();
+        public ReorderableListHandler OnElementAdded = new ReorderableListHandler();
+
+        private RectTransform _content;
+        private ReorderableListContent _listContent;
+
+        public RectTransform Content
+        {
+            get
+            {
+                if (_content == null)
+                {
+                    _content = ContentLayout.GetComponent<RectTransform>();
+                }
+                return _content;
+            }
+        }
+
+        Canvas GetCanvas()
+        {
+            Transform t = transform;
+            Canvas canvas = null;
+        
+
+            int lvlLimit = 100;
+            int lvl = 0;
+
+            while (canvas == null && lvl < lvlLimit)
+            {
+                canvas = t.gameObject.GetComponent<Canvas>();
+                if (canvas == null)
+                {
+                    t = t.parent;
+                }
+
+                lvl++;
+            }
+            return canvas;
+        }
+        
+        public void Init(LayoutGroup content, RectTransform area)
+        {
+            ContentLayout = content;
+
+            DraggableArea = transform.root.GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
+
+            _listContent = ContentLayout.gameObject.AddComponent<ReorderableListContent>();
+            _listContent.Init(this);
+        }
+
+        #region Nested type: ReorderableListEventStruct
+
+        [Serializable]
+        public struct ReorderableListEventStruct
+        {
+            public GameObject DroppedObject;
+            public int FromIndex;
+            public ReorderableList FromList;
+            public bool IsAClone;
+            public GameObject SourceObject;
+            public int ToIndex;
+            public ReorderableList ToList;
+
+            public void Cancel()
+            {
+                SourceObject.GetComponent<ReorderableListElement>().isValid = false;
+            }
+        }
+
+        #endregion
+
+        #region Nested type: ReorderableListHandler
+
+        [Serializable]
+        public class ReorderableListHandler : UnityEvent<ReorderableListEventStruct>
+        {
+        }
+        
+        #endregion
+    }
+}
---
+++ Source/BetterTracking.Unity/Extensions/ReorderableListContent.cs
@@ -0,0 +1,111 @@
+﻿/// Credit Ziboo
+/// Sourced from - http://forum.unity3d.com/threads/free-reorderable-list.364600/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BetterTracking.Unity
+{
+    public class ReorderableListContent : MonoBehaviour
+    {
+        private List<Transform> _cachedChildren;
+        private List<ReorderableListElement> _cachedListElement;
+        private ReorderableListElement _ele;
+        private ReorderableList _extList;
+        private RectTransform _rect;
+
+        private void OnEnable()
+        {
+            if(_rect)StartCoroutine(RefreshChildren());
+        }
+
+        public void OnTransformChildrenChanged()
+        {
+            if(this.isActiveAndEnabled)StartCoroutine(RefreshChildren());
+        }
+
+        public void Init(ReorderableList extList)
+        {
+            _extList = extList;
+            _rect = GetComponent<RectTransform>();
+            _cachedChildren = new List<Transform>();
+            _cachedListElement = new List<ReorderableListElement>();
+
+            StartCoroutine(RefreshChildren());
+        }
+
+        private IEnumerator RefreshChildren()
+        {
+            yield return new WaitForEndOfFrame();
+
+            if (_rect.childCount > 0)
+            {
+                var first = _rect.GetChild(0);
+
+                if (first != null && first.gameObject.name != "Fake")
+                {
+                    VesselGroup group = first.GetComponent<VesselGroup>();
+
+                    int timer = 0;
+
+                    while (group == null)
+                    {
+                        if (timer > 20)
+                            yield break;
+
+                        group = first.GetComponent<VesselGroup>();
+                        timer++;
+
+                        if (group == null)
+                            yield return null;
+                    }
+
+                    while (!group.Initialized || group.Header == null)
+                    {
+                        yield return null;
+                    }
+
+                    //Handle new chilren
+                    for (int i = 0; i < _rect.childCount; i++)
+                    {
+                        if (_rect.GetChild(i) == null)
+                            continue;
+
+                        if (_cachedChildren.Contains(_rect.GetChild(i)))
+                            continue;
+
+                        VesselGroup child = _rect.GetChild(i).GetComponent<VesselGroup>();
+
+                        if (child != null)
+                        {
+                            _ele = child.Header.DragHandle.AddComponent<ReorderableListElement>();
+                            _ele.Init(_extList);
+                        }
+
+                        //Get or Create ReorderableListElement
+                        //_ele = _rect.GetChild(i).gameObject.GetComponent<ReorderableListElement>() ??
+                        //       _rect.GetChild(i).gameObject.AddComponent<ReorderableListElement>();
+                        //_ele.Init(_extList);
+
+                        _cachedChildren.Add(_rect.GetChild(i));
+                        _cachedListElement.Add(_ele);
+                    }
+                }
+            }
+
+            ////HACK a little hack, if I don't wait one frame I don't have the right deleted children
+            yield return null;
+
+            //Remove deleted child
+            for (int i = _cachedChildren.Count - 1; i >= 0; i--)
+            {
+                if (_cachedChildren[i] == null)
+                {
+                    _cachedChildren.RemoveAt(i);
+                    _cachedListElement.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
